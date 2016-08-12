da952b33b6aac59eb6dcdb2b2c3528b7287cb10e
Justyna Bielecka <justynbielecka@gmail.com>
2016-08-12 23:25:16 +0200

    login base funcionality finished

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,24 +1,41 @@
 $(document).foundation()
 $(document).on("ready",function(){
 
+  // Variables
   var btnGo = $(".btn-go");
   var logoClick = $(".img-efi");
+  var loginValidationInfo = $(".login__validation-info");
+
+ //  Objects
   var currentUser = {
     login: "efi",
     password:"",
   }
 
+  logoClick.on("click", () => alert($(this).attr("src")));
+
   btnGo.on("click", (event) => {
     event.preventDefault();
-    var inputUser = $(".login__name").val();
+    let inputUser = $(".login__name").val();
     currentUser.password = inputUser;
     $(".login__name").val('');
 
-    (!inputUser.length) ? alert("password can not be empty") : sendLogin(currentUser);
+    (!inputUser.length) ? appearErrorInfo("password can not be empty") : sendLogin(currentUser);
 
   });
 
-  logoClick.on("click", () => alert($(this).attr("src")));
+  function appearErrorInfo(info) {
+    loginValidationInfo.removeClass("hide");
+    loginValidationInfo.find("span").text(info);
+    setTimeout(addHide, 2000);
+  }
+
+  var addHide = () => { loginValidationInfo.addClass("hide") }
+
+  $(".btn-close-error").on("click", (event) => {
+    event.preventDefault();
+    addHide();
+  });
 
   function sendLogin(currentUser){
     $.ajax({
@@ -27,12 +44,8 @@ $(document).on("ready",function(){
         dataType: "json",
         data: currentUser,
       }).done((response) => {
-          console.log("success",
-          response);
       }).fail((error) => {
-        $(".login-validation-info").removeClass("hide");
-        $(".login-validation-info").text(error);
-        console.log(error);
+        appearErrorInfo(error.responseText.slice(39,60));
       })
     };
 });
